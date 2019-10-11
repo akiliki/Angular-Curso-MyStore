739b2f52052a70dd01612d0d5f552129be6c9aaf
Akiliki <akiliki@gmail.com>
2019-10-11 22:13:30 +0200

    Limpieza de código

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,13 +6,13 @@ export const environment = {
   production: false,
   url_api: 'https://platzi-store.herokuapp.com',
   firebase: {
-    apiKey: '',
-    authDomain: '',
-    databaseURL: '',
-    projectId: '',
-    storageBucket: '',
-    messagingSenderId: '',
-    appId: ''
+    apiKey: 'AIzaSyCmj7ZjdMcNTbpFEuF6xwBKlaQuHsEFp6g',
+    authDomain: 'arregijanaridendastore.firebaseapp.com',
+    databaseURL: 'https://arregijanaridendastore.firebaseio.com',
+    projectId: 'arregijanaridendastore',
+    storageBucket: 'arregijanaridendastore.appspot.com',
+    messagingSenderId: '254479581817',
+    appId: '1:254479581817:web:8fa9159474f2db32f14aa8'
   }
 };
 
