e2862195b0e0124347e294ff17c1b656bfdeae18
akiliki <32521277+akiliki@users.noreply.github.com>
2019-10-11 22:17:16 +0200

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,12 +2,6 @@ export const environment = {
   production: true,
   url_api: 'https://platzi-store.herokuapp.com',
   firebase: {
-    apiKey: 'AIzaSyCmj7ZjdMcNTbpFEuF6xwBKlaQuHsEFp6g',
-    authDomain: 'arregijanaridendastore.firebaseapp.com',
-    databaseURL: 'https://arregijanaridendastore.firebaseio.com',
-    projectId: 'arregijanaridendastore',
-    storageBucket: 'arregijanaridendastore.appspot.com',
-    messagingSenderId: '254479581817',
-    appId: '1:254479581817:web:8fa9159474f2db32f14aa8'
+
   }
 };
